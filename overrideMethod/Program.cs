6a739242d5e03034eecc1f847b53544231e12653
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-06-02 23:31:32 +0900

    0602 실습일지 05번 문제 메서드 생성

--- overrideMethod/Program.cs
+++ overrideMethod/Program.cs
@@ -8,6 +8,51 @@ namespace overrideMethod
 {
     class Program
     {
+        // 매개변수 input을 제곱해서 반환하는 메서드
+        int Power(int input)
+        {
+            return input * input;
+        }
+
+        // 매개변수 input을 count만큼 제곱해서 반환하는 메서드
+        int Power(int input, int count)
+        {
+            int result = input;
+
+            for(int i = 0; i < count; i++)
+            {
+                result = result * input;
+            }
+
+            return result;
+        }
+
+        // 0부터 end 사이의 모든 정수를 더하는 메서드
+        int SumAll(int end)
+        {
+            int sum = 0;
+
+            for(int i = 1; i <= end; i++)
+            {
+                sum = sum + i;
+            }
+
+            return sum;
+        }
+
+        // start부터 end 사이의 모든 정수를 더하는 메서드
+        int SumAll(int start, int end)
+        {
+            int sum = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                sum = sum + i;
+            }
+
+            return sum;
+        }
+
         static void Main(string[] args)
         {
         }
